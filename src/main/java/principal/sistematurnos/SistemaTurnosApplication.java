1024014caf95231ddba9fcc9bd32755a33145ca7
MMarcos00 <mendezmarcosgt@gmail.com>
2025-05-15 19:26:00 -0600

    Semana 1 – Inicialización del proyecto

---
+++ src/main/java/principal/sistematurnos/SistemaTurnosApplication.java
@@ -0,0 +1,13 @@
+package principal.sistematurnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTurnosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaTurnosApplication.class, args);
+    }
+
+}
